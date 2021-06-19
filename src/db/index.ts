47970dcd1b8fd5cd48d0de63850365c12650b29b
Jenn Lara <jnnfrlara@gmail.com>
2021-06-19 12:05:59 -0700

    fix mongo client + err logs

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,21 +1,34 @@
 import { MongoClient } from "mongodb";
 
 const dbString = "mongodb+srv://fantastic:7WqU4pSbXQmNsanP@challenge.6i8a4.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+const dbName = "sample_airbnb";
+const dbCol = "listingsAndReviews";
 
-const client = new MongoClient(dbString);
+const client = new MongoClient(dbString, {
+  useUnifiedTopology: true,
+});
+
 let db: any = "";
 
 // connect is the initial db connection
 export async function connect() {
   const connection = await client.connect();
-  db = connection.db("sample_airbnb");
+  if (connection === null) {
+    console.error("Could not connect to db");
+  }
+  db = connection.db(dbName);
 }
 
-// getDb is the reusable db connection
+// getDeb is the reusable db connection
 export function getDb() {
-  const col = db.collection("listingsAndReviews");
+  const col = db.collection(dbCol);
+  if (col === null) {
+    console.error("Could not retrieve reusable db connection")
+  }
   return col;
 }
+
+
 
 
 
