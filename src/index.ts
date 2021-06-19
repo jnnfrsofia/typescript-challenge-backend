6a928ac09d13fe55e705d19e832665b025bf3676
Jenn Lara <jnnfrlara@gmail.com>
2021-06-18 17:54:52 -0700

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
-import Stays from "./stays";
+import { Stays } from "./stays";
+import { Reviews } from "./reviews";
+import { connect } from "./db";
 
 const port = 3000;
 const app = express();
 
 app.use("/stays", Stays);
+app.use("/reviews", Reviews);
+
+// this is where we initialize the db connection
+connect();
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
+
